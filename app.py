1900f7c9610acd6c8769150d564d6715dfc733dc
Robson Damasceno Rodrigues <78008345+sonata1987@users.noreply.github.com>
2025-11-30 23:25:27 -0300

    Adição de framework SOLID
    Adição de requisitos do  framework SOLID no projeto de mentoria

--- app.py
+++ app.py
@@ -1,32 +1,35 @@
+import dados
 from flask import Flask, make_response, request, jsonify, json
-import data
+from dados import LivroRepository
+
+livro_repo = LivroRepository() 
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/livros', methods=['GET'])
-def get_livros():
+def get_all():
     return make_response(
-        jsonify(data.get_livros())
+        jsonify(livro_repo.get_all())
     )
 
 @app.route('/livros', methods=['POST'])
-def post_livros():
+def post():
     livro_data = request.json
-    livro_inserido = data.add_livro(livro_data) 
+    livro_inserido = livro_repo.add(livro_data) 
     return make_response(jsonify(livro_inserido))
     
 
 @app.route('/livros/<int:id>', methods=['DELETE'])
-def delete_livro(id):
-    data.delete_livro(id)
+def delete(id):
+    livro_repo.delete(id)
     return make_response('', 204)
 
 
 @app.route('/livros/<int:id>', methods=['PATCH'])
-def update_livro(id):
+def update(id):
     livro_data = request.json
-    livro_atualizado = data.update_livro(id, livro_data)
+    livro_atualizado = livro_repo.update(id, livro_data)
     return make_response(jsonify(livro_atualizado))
 
 
